72d70e870352c1624cbbd1c0ef03607712dc5fe0
Darmik Reddy <darmikreddy18@gmail.com>
2026-08-06 15:09:09 +0530

    Connect frontend to production API

--- frontend/mikblogs/src/pages/show/blog.jsx
+++ frontend/mikblogs/src/pages/show/blog.jsx
@@ -163,7 +163,7 @@ const removeComment = (commentId) => {
     </div>
 
                     <img
-                        src={`http://localhost:8080${blog.image.url}`}
+                     src= {`${API_URL}${blog.image.url}`}
                         alt={blog.title}
                         className="blog-cover"
                     />
