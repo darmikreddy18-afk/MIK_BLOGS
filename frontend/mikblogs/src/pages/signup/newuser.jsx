7451bdb6367a51397e59542ab85aa0fd17e70277
Darmik Reddy <darmikreddy18@gmail.com>
2026-08-07 09:55:59 +0530

    third change

--- frontend/mikblogs/src/pages/signup/newuser.jsx
+++ frontend/mikblogs/src/pages/signup/newuser.jsx
@@ -4,6 +4,7 @@ import { Link,useNavigate} from "react-router-dom";
 import { API_URL } from "../../config.js"
 
 function Signup() {
+const[navigate]=useNavigate();
 const [username, setUsername] = useState("");
 const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
